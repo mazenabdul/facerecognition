8b6c5715138740a77984f729b98811b2eaad1f6d
mazenabdul <63692059+mazenabdul@users.noreply.github.com>
2020-05-08 22:15:48 -0400

    Updates

---
+++ precache-manifest.5253db9da7f033f2d7fca610ef818a32.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e667ed965ac36d55ce6c9f7d13d75bf4",
+    "url": "/facerecognition/index.html"
+  },
+  {
+    "revision": "7319cd30c5e444853a05",
+    "url": "/facerecognition/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "32073ffd8be1947482b0",
+    "url": "/facerecognition/static/css/main.fee148d2.chunk.css"
+  },
+  {
+    "revision": "7319cd30c5e444853a05",
+    "url": "/facerecognition/static/js/2.b8f8bb48.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/facerecognition/static/js/2.b8f8bb48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32073ffd8be1947482b0",
+    "url": "/facerecognition/static/js/main.7988f218.chunk.js"
+  },
+  {
+    "revision": "7747f7a09a564ebd257a",
+    "url": "/facerecognition/static/js/runtime-main.8421dbec.js"
+  }
+]);
